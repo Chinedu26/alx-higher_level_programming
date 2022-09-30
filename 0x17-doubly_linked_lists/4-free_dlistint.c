13f39fceff64708c5719df77ce96c815c5746884
Chinedu26 <106815468+Chinedu26@users.noreply.github.com>
2022-10-01 00:59:48 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,10 @@
-#incluse "lists.h"
+#include "lists.h"
 #include <stdlib.h>
+
 /**
  * free_dlistint - free a dlistint_t list
  * @head: pointer to the beginning of the linked list
+ *
  * Return: void
  */
 void free_dlistint(dlistint_t *head)
