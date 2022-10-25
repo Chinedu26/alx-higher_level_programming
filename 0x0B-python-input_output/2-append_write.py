c6c8471d5a099fd8399bd3fda60766bbf9d88972
Chinedu26 <106815468+Chinedu26@users.noreply.github.com>
2022-10-25 22:25:10 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 """
-
-Contains the read_file function
-
+function that appends a string
 """
 
 
-
-
-
-def read_file(filename=""):
-
-        """""reads a text file(UTF8) and prints it to stdout"""
-
-            with open(filename, "r", encoding="utf-8") as f:
-
-                        print(f.read(), end="")
+def append_write(filename="", text=""):
+    """eturns the number of characters added:"""
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
