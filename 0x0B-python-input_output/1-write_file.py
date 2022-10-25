a8f24174f96406c8e1ba944e8c7613d4860c5890
Chinedu26 <106815468+Chinedu26@users.noreply.github.com>
2022-10-25 22:24:19 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 """
-
-Contains the read_file function
-
+Contains the function "wrtie_file"
 """
 
 
-
-
-
-def read_file(filename=""):
-
-        """""reads a text file(UTF8) and prints it to stdout"""
-
-            with open(filename, "r", encoding="utf-8") as f:
-
-                        print(f.read(), end="")
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
